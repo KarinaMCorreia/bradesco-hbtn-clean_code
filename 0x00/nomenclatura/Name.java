74960298c335ad6434d7eeb65547b3e011e9551c
KarinaMCorreia <ka.mendes.correia@gmail.com>
2025-11-18 00:57:53 -0300

    feat: implementar classe Name seguindo boas práticas de nomenclatura

--- 0x00/nomenclatura/Name.java
+++ 0x00/nomenclatura/Name.java
@@ -4,40 +4,31 @@ import java.net.URL;
 
 public class Name {
 
-    // Classe usada nas assinaturas dos métodos
     public static class User {
     }
 
-    // Classe de apoio para o retorno da busca
     public static class Testcase {
     }
 
-    // Classe de apoio para a lista de contas
     public static class Account {
     }
 
-    // variáveis que falam por si só
     public Date modifiedDate;
 
     public List<Testcase> findAllTestcasesByUser(Name.User user) {
         return null;
     }
 
-    // Declaração correta usando List
     public List<Account> AccountList;
 
-    // Sem prefixos no parâmetro
     public void deleteUser(Name.User user) {
     }
 
-    // Evite mapa mental: nome claro
     public URL url;
 
-    // Métodos com verbo
     public void saveCar() {
     }
 
-    // Uma palavra por conceito: mesma ação, mesmo verbo
     public void saveUser() {
     }
 
@@ -47,7 +38,6 @@ public class Name {
     public void saveContract() {
     }
 
-    // Constantes em vez de "números mágicos"
-    public final int DAYS_IN_YEAR = 360;
-    public final int HOURS_IN_WEEK = 168;
+    public static final int DAYS_IN_YEAR = 360;
+    public static final int HOURS_IN_WEEK = 168;
 }
